5070a86335a08fe32c071c517f6154ca41125bd2
Gul Zaman <61613273+Zamangithub@users.noreply.github.com>
2022-09-02 21:05:06 +0500

    Add files via upload

---
+++ registration/src/main/java/com/registration/email/EmailSender.java
@@ -0,0 +1,6 @@
+package com.registration.email;
+
+public interface EmailSender {
+
+     void send(String to,String email);
+}
---
+++ registration/src/main/java/com/registration/registrationprocess/RegistrationRequest.java
@@ -0,0 +1,15 @@
+package com.registration.registrationprocess;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+
+}
